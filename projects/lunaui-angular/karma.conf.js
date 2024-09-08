14abc1c8b7d99fd1c9ed0a2f084b6e88673175b3
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-09-08 13:09:06 -0500

    chore: Fix lint error

--- projects/lunaui-angular/karma.conf.js
+++ projects/lunaui-angular/karma.conf.js
@@ -1,6 +1,6 @@
+/* eslint-disable */
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: "",
