989c26fc7e5fbd7fd86651c6b4e571b469279962
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-09-17 20:25:59 -0500

    Feat: Button

--- projects/lunaui-angular/src/lib/buttons/button/button.component.ts
+++ projects/lunaui-angular/src/lib/buttons/button/button.component.ts
@@ -1,14 +1,32 @@
-import { Component, Input } from '@angular/core';
-import { LunaBtnSizes, LunaBtnTypes } from './button';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { BtnType, LunaBtnSizes, LunaBtnVariants, PopoverTargetAction } from './button';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'luna-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styles: [],
 })
-export class ButtonComponent {
-  @Input() severity: LunaBtnTypes = 'primary'
+export class LunaButtonComponent {
   @Input() size: LunaBtnSizes = 'medium'
+  @Input() variant: LunaBtnVariants = 'filled'
+  @Input() ariaLabel: string = ''
+  @Input() ariaLive: 'off' | 'polite' | 'assertive' | '' = '';
+  @Input() ariaDescribedBy: string = '';
+  @Input() disabled: boolean = false;
+  @Input() type: BtnType = 'button';
+  @Input() form: string = '';
+  @Input() name: string = '';
+  @Input() popovertarget: string = '';
+  @Input() popovertargetaction: PopoverTargetAction | null = null;
+  @Input() className: string = '';
+  @Input() styles: Record<string, string> = {}
+  @Input() value: string | null = null;
+
+  @Output() onClick = new EventEmitter<Event>()
+  @Output() focus = new EventEmitter<FocusEvent>()
+  @Output() blur = new EventEmitter<FocusEvent>()
 }
